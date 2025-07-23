Traning. Weekend training scheme. Prior training bug fixed. Mental map normalization.

Analysis/packFuncForShap.py
@@ -156,12 +156,12 @@ def sparseBackground(dataset: np.array, visited_id: list = None):
     return background_uni, background_weight, background_iden
     
 
-def modelRewardExplain(date: int, who: int, binary_be_vs_loc = True, blank = True):
+def modelRewardExplain(date: int, who: int, binary_be_vs_loc = True, blank = True, model_dir = './model/'):
     '''
         Give the SHAP value by grouping the type.
     '''
     print('Explaining person: {who:9d}, date: {date}'.format(who=who, date=date))
-    model = SIRLU.loadModel(who=who, date=date)
+    model = SIRLU.loadModel(who=who, date=date, model_dir=model_dir)
 
     dataset, visited_id = backgroundData(who=who, date=date)
     dataset_uni, dataset_freq, dataset_iden = sparseBackground(dataset, visited_id)
@@ -219,7 +219,7 @@ def modelUserDateCombination(by_week=True):
     Extract the user and date combination from the model directory.
     Each target for every seven days.
     '''
-    model_dir = './model/'
+    # model_dir = './model/'
     # list all users with folder name consisting of all digits.
     # note: change here
     user_list = [name for name in os.listdir(model_dir) if name.isdigit()]
@@ -228,7 +228,7 @@ def modelUserDateCombination(by_week=True):
     for user in user_list:
         evolution_model_path = model_dir + user + '/' + 'evolution_model/'
         date_list = [int(params.rstrip('.pickle')[-8:]) for params in os.listdir(evolution_model_path)]
-        date_list = SIRLU.extract_week_ends(date_list)[0] if by_week else date_list[::7] # similarly we can change to 1
+        date_list = SIRLU.extract_week_ends(date_list)[0] if by_week else date_list[::1] # similarly we can change to 1
         for date in date_list:
             combination.append((int(user), date))
     return combination
@@ -239,14 +239,14 @@ def modelDateOfUser(user, by_week = True):
     Extract the date list of the user.
     Each target for every seven days.
     '''
-    model_dir = './model/'
+    # model_dir = './model/'
     user = SIRLU.toWhoString(user)
     evolution_model_path = model_dir + user + '/' + 'evolution_model/'
     date_list = [int(params.rstrip('.pickle')[-8:]) for params in os.listdir(evolution_model_path)]
     if by_week:
         date_list, _ = SIRLU.extract_week_ends(date_list)
     else:
-        date_list = date_list[::7] # if the model is not continuously trained, change 7 to 1.
+        date_list = date_list[::1] # if the model is not continuously trained, change 7 to 1.
     return date_list
 
 def explainOneUser(user, parallel=False, binary_be_vs_loc=True, blank=True):
@@ -256,11 +256,12 @@ def explainOneUser(user, parallel=False, binary_be_vs_loc=True, blank=True):
         shap_dict = dict()
         # add reverse to mitigate the load balancing problem.
         for date in reversed(date_list):
-            shap_dict[date] = modelRewardExplain(date, who=user, binary_be_vs_loc=binary_be_vs_loc, blank=blank)
+            shap_dict[date] = modelRewardExplain(date, who=user, binary_be_vs_loc=binary_be_vs_loc, blank=blank, 
+                                                 model_dir=model_dir)
     else:
         # parallel version
-        CPU_COUNT = min(len(date_list), mp.cpu_count() - 1)
-        combination = [(date, user, binary_be_vs_loc, blank) for date in reversed(date_list)]
+        CPU_COUNT = min(len(date_list),30)
+        combination = [(date, user, binary_be_vs_loc, blank, model_dir) for date in reversed(date_list)]
         # set start method to spawn to avoid the error of fork
         mp.set_start_method('spawn', force=True)
         with mp.Pool(CPU_COUNT) as pool:
@@ -287,12 +288,12 @@ def explainAllRewards(parallel = False, binary_be_vs_loc=True, blank=True):
             shap_dict[(user, date)] = shap_dict_values[idx]
     return shap_dict
 
-def modelRewardBaselineCalculation(date: int, who: int):
+def modelRewardBaselineCalculation(date: int, who: int, model_dir = './model/'):
     '''
         Give the SHAP value by grouping the type.
     '''
     print('Explaining person: {who:8d}, date: {date}'.format(who=who, date=date))
-    model = SIRLU.loadModel(who=who, date=date)
+    model = SIRLU.loadModel(who=who, date=date, model_dir=model_dir)
     # modelPredWrapper = partial(modelPredict, model=model, attribute_type='reward')
 
     dataset, visited_id = backgroundData(who=who, date = date)
@@ -325,18 +326,18 @@ if __name__ == '__main__':
     '''
     Half Parallel Version
     '''
-    model_dir = './model/'
+    model_dir = './model_training_weekend_with_prior/'
     user_list = [int(name) for name in os.listdir(model_dir) if name.isdigit()]
     user_list.sort()
     for user in user_list:
         # note: remember to change back
         res = explainOneUser(user, parallel=True, binary_be_vs_loc=False, blank=True)
-        with open('./product/shap_res_{:09d}.pkl'.format(user), 'wb') as f:
+        with open('./product/shap_weekend/shap_res_{:09d}.pkl'.format(user), 'wb') as f:
             pickle.dump(res, f)
     '''
     By Hand
     '''
-    # model_dir = './model/'
+    # model_dir = './model_training_skip_10_with_prior/'
     # user_list = [int(name) for name in os.listdir(model_dir) if name.isdigit()]
     # user_list.sort()
 

Analysis/predictionTask.py
@@ -113,75 +113,73 @@ def visitedDate(all_traj_path='./data/all_traj.json'):
     visited_dates = [d.get('date') for d in loaded_dicts_all]
     return visited_dates
 
-# def personPredictEvaluation(who: int):
-#     id_coords_mapping = SIRLU.load_id_coords_mapping(who)
-#     traveler = SIRLU.load_traveler(who)
+def personInterpretEvaluationWeekend(who: int, period: str = 'future', model_dir: str = './model/'):
+    trained_models_path = model_dir + SIRLU.toWhoString(who) + '/' + 'evolution_model/'
+    evoludate = [int(s.split('_')[-1].replace('.pickle', '')) for s in os.listdir(trained_models_path)]
+    evoludate.sort()
+
+    re_contrib_list = []
+    exp_contrib_list = []
+    pr_contrib_list = []
+    acu_contrib_list = []
+    for i, one_evolution_date in enumerate(evoludate):
+        # find the position of the evolution date
+        print("The evolution date is: ", one_evolution_date)
+        # 根据period参数选择时间段范围
+        if period == 'future':
+            # 'future' period configuration
+            # if the buffer range is out of range, break the loop
+            if i == len(evoludate) - 1: break
+            # create the date range for evaluation
+            # know the start and end date for evaluation func
+            start_date = evoludate[i]
+            end_date = evoludate[-1]
+        elif period == 'total':
+            # 'total' period configuration
+            start_date = evoludate[0]
+            end_date = evoludate[-1]
+        else:
+            raise ValueError("Invalid period argument. Use 'future' or 'total'.")
+        
+        prior_model = loadModel(who=who, model_dir=model_dir)
+
+        if i == 0:
+            recent_knowledge_model = prior_model
+        else:
+            prior_evolution_date = evoludate[i - 1]
+            recent_knowledge_model = loadModel(who=who, date=prior_evolution_date, prior=True, model_dir=model_dir)
+
+        recent_experience_model = loadModel(who=who, date=one_evolution_date, prior=False, model_dir=model_dir)
+
+        accumulated_experience_model = loadModel(who=who, date=one_evolution_date, prior=False, accumulate=True, model_dir=model_dir)
+        
+        complete_model = loadModel(who=who, date=one_evolution_date, prior=True, model_dir=model_dir)
+
+        tabula_rasa_model = loadModel(who=who, tabular=True, model_dir=model_dir)
+
+        df_recent = modelEvaluation(recent_knowledge_model, who, start_date, end_date, mode='reward')
+        df_experience = modelEvaluation(recent_experience_model, who, start_date, end_date, mode='reward')
+        df_prior = modelEvaluation(prior_model, who, start_date, end_date, mode='reward')
+        df_accumknow = modelEvaluation(accumulated_experience_model, who, start_date, end_date, mode='reward')
+        df_complete = modelEvaluation(complete_model, who, start_date, end_date, mode='reward')
+        df_tabular = modelEvaluation(tabula_rasa_model, who, start_date, end_date, mode='reward')
+        
+        recen_contrib = 0.5 * stepwise_kl_div_compute(df_tabular, df_recent) + 0.5 * stepwise_kl_div_compute(df_experience, df_complete)
+        exper_contrib = 0.5 * stepwise_kl_div_compute(df_tabular, df_experience) + 0.5 * stepwise_kl_div_compute(df_recent, df_complete)
+        prior_contrib = 0.5 * stepwise_kl_div_compute(df_tabular, df_prior) + 0.5 * stepwise_kl_div_compute(df_accumknow, df_complete)
+        accum_contrib = 0.5 * stepwise_kl_div_compute(df_tabular, df_accumknow) + 0.5 * stepwise_kl_div_compute(df_prior, df_complete)
+        pr_contrib_list.append(prior_contrib); re_contrib_list.append(recen_contrib)
+        exp_contrib_list.append(exper_contrib); acu_contrib_list.append(accum_contrib)
     
-#     migrtdate = traveler.migrt
-#     visitdate = traveler.visit_date
-#     # create a list of dates for evolution evaluation
-#     evoludate = list(filter(lambda d: d >= migrtdate, visitdate))
-#     # create buffer range: 10 future days and the date itself
-#     evolution_buffer = 10 + 1
+    # result in the form of a dataframe
+    result_df = pd.DataFrame({'date': evoludate[:len(pr_contrib_list)], 
+                              're_contrib': re_contrib_list,
+                              'exp_contrib': exp_contrib_list,
+                              'pr_contrib': pr_contrib_list, 
+                              'acu_contrib': acu_contrib_list,
+                              })
+    return result_df
 
-#     pr_contrib_list = []
-#     exp_contrib_list = []
-#     pr_log_contrib_list = []
-#     exp_log_contrib_list = []
-    
-#     for one_evolution_date in evoludate:
-#         # find the position of the evolution date
-#         evolution_pos = evoludate.index(one_evolution_date)
-#         # if the buffer range is out of range, break the loop
-#         if evolution_pos + evolution_buffer > len(evoludate):
-#             break
-#         print("The evolution date is: ", one_evolution_date)
-        
-#         # create the date range for evaluation
-#         evolution_date_range = evoludate[evolution_pos : evolution_pos + evolution_buffer]
-#         # know the start and end date for evaluation func
-#         start_date = evolution_date_range[0]
-#         end_date = evolution_date_range[-1]
-        
-#         if evolution_pos < 10:
-#             prior_model = loadModel(who=who, )
-#         else:
-#             prior_evolution_date = evoludate[evolution_pos - 10]
-#             prior_model = loadModel(who=who, date=prior_evolution_date, prior=True)
-
-#         experience_model = loadModel(who=who, date=one_evolution_date, prior=False)
-
-#         complete_model = loadModel(who=who, date=one_evolution_date, prior=True)
-
-#         tabula_rasa_model = loadModel(who=who, tabular=True)
-
-#         state_attribute = SIRLU.load_state_attrs(who, before=False)
-        
-#         mse_pr, acc_pr = modelEvaluation(prior_model, who, start_date, end_date, mode='action', coords_id=id_coords_mapping)
-#         mse_exp, acc_exp = modelEvaluation(experience_model, who, start_date, end_date, mode='action', coords_id=id_coords_mapping)
-#         mse_both, acc_both = modelEvaluation(complete_model, who, start_date, end_date, mode='action', coords_id=id_coords_mapping)
-#         mse_non, acc_non = modelEvaluation(tabula_rasa_model, who, start_date, end_date, mode='action', coords_id=id_coords_mapping)
-        
-#         # compute the contribution based on mse, according to nexus paper
-#         pr_log_contrib = 0.5 * (np.log10(mse_exp/mse_both) + np.log10(mse_non/mse_pr))
-#         pr_log_contrib_list.append(pr_log_contrib)
-#         exp_log_contrib = 0.5 * (np.log10(mse_pr/mse_both) + np.log10(mse_non/mse_exp))
-#         exp_log_contrib_list.append(exp_log_contrib)
-
-#         # compute the contribution based on accuracy
-#         pr_contrib = 0.5 * (acc_both - acc_exp + acc_pr - acc_non)
-#         pr_contrib_list.append(pr_contrib)
-#         exp_contrib = 0.5 * (acc_both - acc_pr + acc_exp - acc_non)
-#         exp_contrib_list.append(exp_contrib)
-        
-#     # result in the form of a dataframe
-#     result_df = pd.DataFrame({'date': evoludate[:len(pr_contrib_list)], 
-#                               'pr_acc_contrib': pr_contrib_list, 
-#                               'exp_acc_contrib': exp_contrib_list, 
-#                               'pr_mse_contrib': pr_log_contrib_list, 
-#                               'exp_mse_contrib': exp_log_contrib_list})
-    
-#     return result_df
 
 
 def stepwise_kl_div_compute(df_1: pd.DataFrame, df_2: pd.DataFrame):
@@ -195,7 +193,7 @@ def stepwise_kl_div_compute(df_1: pd.DataFrame, df_2: pd.DataFrame):
     return kl_series.mean()
 
 
-def personInterpretEvaluation(who: int, period: str = 'future'):
+def personInterpretEvaluation(who: int, period: str = 'future', model_dir: str = './model/'):
     traveler = SIRLU.load_traveler(who)
     migrtdate = SIRLU.load_traveler(who).iter_start_date
 
@@ -234,20 +232,20 @@ def personInterpretEvaluation(who: int, period: str = 'future'):
         else:
             raise ValueError("Invalid period argument. Use 'future' or 'total'.")
         
-        prior_model = loadModel(who=who, )
+        prior_model = loadModel(who=who, model_dir=model_dir)
         if evolution_pos < 10:
             recent_knowledge_model = prior_model
         else:
             prior_evolution_date = evoludate[evolution_pos - 10]
-            recent_knowledge_model = loadModel(who=who, date=prior_evolution_date, prior=True)
+            recent_knowledge_model = loadModel(who=who, date=prior_evolution_date, prior=True, model_dir=model_dir)
 
-        recent_experience_model = loadModel(who=who, date=one_evolution_date, prior=False)
+        recent_experience_model = loadModel(who=who, date=one_evolution_date, prior=False, model_dir=model_dir)
 
-        accumulated_experience_model = loadModel(who=who, date=one_evolution_date, prior=False, accumulate=True)
+        accumulated_experience_model = loadModel(who=who, date=one_evolution_date, prior=False, accumulate=True, model_dir=model_dir)
         
-        complete_model = loadModel(who=who, date=one_evolution_date, prior=True)
+        complete_model = loadModel(who=who, date=one_evolution_date, prior=True, model_dir=model_dir)
 
-        tabula_rasa_model = loadModel(who=who, tabular=True)
+        tabula_rasa_model = loadModel(who=who, tabular=True, model_dir=model_dir)
 
         df_recent = modelEvaluation(recent_knowledge_model, who, start_date, end_date, mode='reward')
         df_experience = modelEvaluation(recent_experience_model, who, start_date, end_date, mode='reward')
@@ -276,22 +274,22 @@ def personInterpretEvaluation(who: int, period: str = 'future'):
 
 if __name__ == "__main__":
 
-    
-    wholist = [int(f) for f in os.listdir('./model/') if f.isdigit()]
+    model_dir = './model_training_weekend_with_prior/'
+    wholist = [int(f) for f in os.listdir(model_dir) if f.isdigit()]
     wholist.sort()
-    wholist = wholist[:1]
-    params_list = [(who, 'total') for who in wholist]
+    # wholist = wholist[:1]
+    params_list = [(who, 'total', model_dir) for who in wholist]
     '''
     Parallel Verison
     '''
     import multiprocessing as mp
-    CPU_COUNT = len(wholist)
-    # CPU_COUNT = 16
+    # CPU_COUNT = len(wholist)
+    CPU_COUNT = 16
     mp.set_start_method('spawn', force=True)
     with mp.Pool(CPU_COUNT) as pool:
-        iterDfs = pool.starmap(personInterpretEvaluation, params_list)
+        iterDfs = pool.starmap(personInterpretEvaluationWeekend, params_list)
 
-    with open('./product/iterationEvo_TOTAL.pkl', 'wb') as file:
+    with open('./product/knowledge_weekend/iterationEvo_TOTAL.pkl', 'wb') as file:
         pickle.dump(iterDfs, file)
     
     '''
@@ -300,6 +298,6 @@ if __name__ == "__main__":
     # i = 0
     # who = wholist[i]
     # res = dict()
-    # res[who] = personInterpretEvaluation(who, 'total')    
+    # res[who] = personInterpretEvaluationWeekend(who, 'total', model_dir=model_dir)    
     # with open('./product/interpretEvo_{:09d}.pkl'.format(who), 'wb') as file:
     #     pickle.dump(res, file)    

SCBIRL_Global_PE/migrationProcess.py
@@ -3,10 +3,11 @@ import jax.numpy as np
 import numpy as onp
 import pickle
 import os
+import copy 
 import pandas as pd
 from tqdm import tqdm
 
-from .utils import getActionDim, loadJsonFile, loadTravelDataFromDicts, preprocessStateAttributes, plugInDataPair
+from .utils import getActionDim, loadJsonFile, loadTravelDataFromDicts, preprocessStateAttributes, plugInDataPair, extract_week_ends
 from scipy.special import softmax
 
 def getComputeFunction(model, attribute_type):
@@ -60,20 +61,24 @@ def readAndPrepareData(user_data_path, start_date):
 
     return visitedState, trajInitChains, trajIterChains, stateAttribute
 
-def afterMigrt(model, dataPath, outputPath, start_date, iter_type):
+def afterMigrt(model, dataPath, outputPath, start_date, iter_type, prior_iter=True, by_weekend=False):
     '''
     Iteratively train the model with real traj data.
     '''
-    assert iter_type in ['recent', 'prior'], "Argument `iter_type` should be either 'recent' or 'prior'."
-    if iter_type == 'recent':
-        model_tag = 'iterated'
-        folder_name = "evolution_model/"
+    
+    if prior_iter:
+        assert iter_type in ['recent', 'incremental'], "Argument `iter_type` should be either 'recent' or 'incremental'."
+        if iter_type == 'recent':
+            model_tag = 'iterated'
+            folder_name = "evolution_model/"
+        else:
+            model_tag = 'increased'
+            folder_name = 'empirical_model/'
     else:
-        model_tag = 'increased'
-        folder_name = 'empirical_model/'
+        model_tag = 'ignorant'
+        folder_name = 'no_prior_model/'
     
     full_traj_path = dataPath + "all_traj.json"
-
     # Load the mapping between IDs and their corresponding fnid.
     with open(dataPath + "id_coords_mapping.pkl", "rb") as f:
         id_coords = pickle.load(f)
@@ -86,36 +91,212 @@ def afterMigrt(model, dataPath, outputPath, start_date, iter_type):
     # Read and preprocess data for analysis.
     visitedState, trajInitChains, trajIterChains, stateAttribute = readAndPrepareData(dataPath, start_date)
 
-    # Initialize an empty DataFrame with predefined columns
-    resultsDf = pd.DataFrame(columns=['coords', 'fnid'])
-    # Iterate over the coords_fnid dictionary and append each key-value pair to resultsDf
-    for key, value in coords_fnid.items():
-        # Append the key-value pair as a new row to resultsDf
-        resultsDf = resultsDf._append({'coords': key, 'fnid': value}, ignore_index=True)
-
 
     modelDir = outputPath + folder_name
     if not os.path.exists(modelDir):
         os.makedirs(modelDir)
-    memory_buffer = 10 - 1 
+    
+    current_model = model
+    if by_weekend:
+        date_list = [tc.date for tc in trajIterChains]
+        weekends, week_codes = extract_week_ends(date_list)
+        week_codes_unique = list(set(week_codes))
 
-    for i in range(0, len(trajIterChains), 1):
+        for w in sorted(week_codes_unique):
+            if not prior_iter:
+                current_model = copy.deepcopy(model)
+            iter_training_set = [trajIterChains[i] for i, week in enumerate(week_codes) if week == w]
 
-        if i < memory_buffer:
-            iter_training_set = trajInitChains[-(memory_buffer-i):] + trajIterChains[:i]
+            # Process and calculate reward values after migration.
+            plugInDataPair(iter_training_set, stateAttribute, current_model, visitedState)
+
+            # Train the model.
+            # change
+            # weights = [1 / 2 ** (memory_buffer - i) for i in range(memory_buffer)]
+            weights = None
+            current_model.train(iters=1000, loss_threshold=0.005, weights=weights, prior=True)
+
+            # Save the current model state.
+            modelSavePath = modelDir + model_tag + '_model_' + str(iter_training_set[-1].date) + ".pickle"
+            current_model.modelSave(modelSavePath)
+        
+    else:
+        memory_buffer = 10 - 1 
+        for i in range(0, len(trajIterChains), 1):
+            if not prior_iter:
+                current_model = copy.deepcopy(model)
+                
+            if i < memory_buffer:
+                iter_training_set = trajInitChains[-(memory_buffer-i):] + trajIterChains[:i]
+            else:
+                iter_training_set = trajIterChains[i-memory_buffer:i]
+            iter_training_set = iter_training_set + [trajIterChains[i]]
+
+            # Process and calculate reward values after migration.
+            plugInDataPair(iter_training_set, stateAttribute, model, visitedState)
+
+            # Train the model.
+            # change
+            # weights = [1 / 2 ** (memory_buffer - i) for i in range(memory_buffer)]
+            weights = None
+            model.train(iters=1000, loss_threshold=0.005, weights=weights, prior=True)
+
+            # Save the current model state.
+            modelSavePath = modelDir + model_tag + '_model_' + str(iter_training_set[-1].date) + ".pickle"
+            model.modelSave(modelSavePath)
+
+
+# def afterMigrtWeekend(model, dataPath, outputPath, start_date, iter_type):
+#     '''
+#     Iteratively train the model with real traj data.
+#     '''
+#     assert iter_type in ['recent', 'prior'], "Argument `iter_type` should be either 'recent' or 'prior'."
+#     if iter_type == 'recent':
+#         model_tag = 'iterated'
+#         folder_name = "evolution_model/"
+#     else:
+#         model_tag = 'increased'
+#         folder_name = 'empirical_model/'
+    
+#     full_traj_path = dataPath + "all_traj.json"
+
+#     # Load the mapping between IDs and their corresponding fnid.
+#     with open(dataPath + "id_coords_mapping.pkl", "rb") as f:
+#         id_coords = pickle.load(f)
+#     with open(dataPath + "coords_fnid_mapping.pkl", "rb") as f:
+#         coords_fnid = pickle.load(f)
+
+#     all_chains = loadTravelDataFromDicts(loadJsonFile(full_traj_path))
+#     actionDim = getActionDim(all_chains)
+
+#     # Read and preprocess data for analysis.
+#     visitedState, trajInitChains, trajIterChains, stateAttribute = readAndPrepareData(dataPath, start_date)
+
+#     # Initialize an empty DataFrame with predefined columns
+#     resultsDf = pd.DataFrame(columns=['coords', 'fnid'])
+#     # Iterate over the coords_fnid dictionary and append each key-value pair to resultsDf
+#     for key, value in coords_fnid.items():
+#         # Append the key-value pair as a new row to resultsDf
+#         resultsDf = resultsDf._append({'coords': key, 'fnid': value}, ignore_index=True)
+
+
+#     modelDir = outputPath + folder_name
+#     if not os.path.exists(modelDir):
+#         os.makedirs(modelDir)
+
+#     date_list = [tc.date for tc in trajIterChains]
+#     weekends, week_codes = extract_week_ends(date_list)
+#     week_codes_unique = list(set(week_codes))
+
+#     for w in sorted(week_codes_unique):
+#         iter_training_set = [trajIterChains[i] for i, week in enumerate(week_codes) if week == w]
+
+#         # Process and calculate reward values after migration.
+#         plugInDataPair(iter_training_set, stateAttribute, model, visitedState)
+
+#         # Train the model.
+#         # change
+#         # weights = [1 / 2 ** (memory_buffer - i) for i in range(memory_buffer)]
+#         weights = None
+#         model.train(iters=1000, loss_threshold=0.005, weights=weights, prior=True)
+
+#         # Save the current model state.
+#         modelSavePath = modelDir + model_tag + '_model_' + str(iter_training_set[-1].date) + ".pickle"
+#         model.modelSave(modelSavePath)
+        
+
+def iterative_model_training(
+    model,
+    dataPath,
+    outputPath,
+    start_date,
+    prior_iter=True,
+    by_weekend=False,
+    iter_type='recent',
+    skip_step=None,
+    memory_span=10,
+    train_iters=1000,
+    loss_threshold=0.005
+):
+    """
+    统一的模型迭代训练函数。
+    参数：
+        model: 初始模型（有先验时为原始模型，无先验时为tabula rasa模型）
+        dataPath: 用户数据路径
+        outputPath: 输出路径
+        start_date: 迁移起始日期
+        prior_iter: 是否有先验迭代（True=有先验，False=无先验）
+        by_weekend: 是否按周迭代（True=按周，False=按天）
+        iter_type: 'prior' 或 'recent'，决定模型保存路径和tag
+        memory_buffer: 记忆窗口长度
+        train_iters: 每次训练迭代次数
+        loss_threshold: 训练loss阈值
+    """
+    import copy
+    import pandas as pd
+    import os
+    
+    # 数据加载
+    full_traj_path = dataPath + "all_traj.json"
+    with open(dataPath + "id_coords_mapping.pkl", "rb") as f:
+        id_coords = pickle.load(f)
+    with open(dataPath + "coords_fnid_mapping.pkl", "rb") as f:
+        coords_fnid = pickle.load(f)
+    all_chains = loadTravelDataFromDicts(loadJsonFile(full_traj_path))
+    actionDim = getActionDim(all_chains)
+    visitedState, trajInitChains, trajIterChains, stateAttribute = readAndPrepareData(dataPath, start_date)
+
+    # 结果目录和tag
+    if prior_iter:
+        if iter_type == 'recent':
+            model_tag = 'iterated'
+            folder_name = "evolution_model/"
+        elif iter_type == 'incremental':
+            model_tag = 'increased'
+            folder_name = 'empirical_model/'
         else:
-            iter_training_set = trajIterChains[i-memory_buffer:i]
-        iter_training_set = iter_training_set + [trajIterChains[i]]
+            raise ValueError("iter_type should be either 'recent' or 'prior'.")
+    else:
+        model_tag = 'ignorant'
+        folder_name = 'no_prior_model/'
+    modelDir = outputPath + folder_name
+    if not os.path.exists(modelDir):
+        os.makedirs(modelDir)
 
-        # Process and calculate reward values after migration.
-        plugInDataPair(iter_training_set, stateAttribute, model, visitedState)
+    # 生成迭代单元
+    if by_weekend:
+        date_list = [tc.date for tc in trajIterChains]
+        weekends, week_codes = extract_week_ends(date_list)
+        week_codes_unique = list(set(week_codes))
+        iter_units = []
+        for w in sorted(week_codes_unique):
+            indices = [i for i, week in enumerate(week_codes) if week == w]
+            iter_units.append([trajIterChains[i] for i in indices])
+        iter_unit_dates = [unit[-1].date for unit in iter_units]
+    else:
+        iter_units = []
+        memory_buffer = memory_span - 1
+        skip = skip_step if skip_step is not None else 1
+        for i in range(0, len(trajIterChains), skip):
+            if i < memory_buffer:
+                iter_training_set = trajInitChains[-(memory_buffer-i):] + trajIterChains[:i]
+            else:
+                iter_training_set = trajIterChains[i-memory_buffer:i]
+            iter_training_set = iter_training_set + [trajIterChains[i]]
+            iter_units.append(iter_training_set)
+        iter_unit_dates = [unit[-1].date for unit in iter_units]
 
-        # Train the model.
-        # change
-        # weights = [1 / 2 ** (memory_buffer - i) for i in range(memory_buffer)]
-        weights = None
-        model.train(iters=1000, loss_threshold=0.005, weights=weights, prior=True)
-
-        # Save the current model state.
-        modelSavePath = modelDir + model_tag + '_model_' + str(iter_training_set[-1].date) + ".pickle"
-        model.modelSave(modelSavePath)
+    # 迭代训练
+    current_model = model
+    for idx, iter_training_set in enumerate(iter_units):
+        # 选择模型
+        if not prior_iter:
+            current_model = copy.deepcopy(model)
+        # 数据注入
+        plugInDataPair(iter_training_set, stateAttribute, current_model, visitedState)
+        # 训练
+        current_model.train(iters=train_iters, loss_threshold=loss_threshold, weights=None, prior=prior_iter)
+        # 保存
+        modelSavePath = modelDir + f'{model_tag}_model_{iter_unit_dates[idx]}.pickle'
+        current_model.modelSave(modelSavePath) 
+        